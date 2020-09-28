edd144366aa25afabeb7434c754fc5a6de869d89
diogo <diogosbarbosa93@gmail.com>
2020-09-27 23:52:02 -0300

    function cria cobra

--- snake-game/script.js
+++ snake-game/script.js
@@ -1,10 +1,29 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+let snake = [];
+snake[0]={
+    x: 8 * box,
+    y: 8 * box
+}
+
+let direction = "right";
 
 function criarBG() {
-    context.fillstyle = 'lightgreen';
+    context.fillstyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
-criarBG();
+function criarCobra(){
+    for(i=0; i < snake.length; i++){
+        context.fillstyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
+function iniciarJogo(){
+    criarBG();
+    criarCobra();
+     
+}
+
