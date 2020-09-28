7439e40861d6eefd98797ea050deacd7c22e2002
diogo <diogosbarbosa93@gmail.com>
2020-09-28 00:52:04 -0300

    correção de direção

--- snake-game/script.js
+++ snake-game/script.js
@@ -41,10 +41,17 @@ function update(event){
 
 function iniciarJogo(){
 
-    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
-    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
-    if(snake[0].y > 15 * box direction == "down")snake[0].y = 0;
-    if(snake[0].y < 0 && direction == "up")snake[0].y = 16 *box;
+    if(snake[0].x > 15 * box && direction == 'right') snake[0].x = 0;
+    if(snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == 'down')snake[0].y = 0;
+    if(snake[0].y < 0 && direction == 'up')snake[0].y = 16 *box;
+
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over :(');
+        }
+    }
 
     criarBG();
     criarCobra();    
@@ -54,11 +61,16 @@ function iniciarJogo(){
     let snakeY = snake[0].y;
 
     if(direction == "right") snakeX += box;
-    if(direction == "left") snakeY -= box;
+    if(direction == "left") snakeX -= box;
     if(direction == "up") snakeY -= box;
-    if(direction == "down") snakeX += box;
+    if(direction == "down") snakeY += box;
 
+    if(snakeX != food.x || snakeY != food.y){
     snake.pop();
+    }else{
+        food.x = Math.floor(Math.random() * 15 +1) * box;
+        food.y = Math.floor(Math.random() * 15 +1) * box;
+    }
 
     let newHead = {
         x: snakeX,
