bf3ec8b2a8e7495f9f769545167f5d3e924b5ce9
diogo <diogosbarbosa93@gmail.com>
2020-09-28 00:28:18 -0300

    function definir direção

--- snake-game/script.js
+++ snake-game/script.js
@@ -8,20 +8,44 @@ snake[0]={
 }
 
 let direction = "right";
+let food ={
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG() {
-    context.fillstyle = "lightgreen";
+    context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
 function criarCobra(){
     for(i=0; i < snake.length; i++){
-        context.fillstyle = "green";
+        context.fillStyle = "black";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect()
+}
+
+document.addEventListener('keydown', update);
+
+function update(event){
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down")direction= "up";
+    if(event.keyCode == 39 && direction != "left")direction= "right";
+    if(event.keyCode == 40 && direction != "up")direction= "down";
+}
+
 function iniciarJogo(){
+
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box direction == "down")snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up")snake[0].y = 16 *box;
+
     criarBG();
     criarCobra();    
 
@@ -40,7 +64,7 @@ function iniciarJogo(){
         y: snakeY
     }
 
-    snake.unshift(newHead);
+    snake.unshift(newHead); 
 }
 
 let jogo = setInterval(iniciarJogo, 100);
