38bba84a06c5a5e2735ce4e6b101ddb07e565a55
diogo <diogosbarbosa93@gmail.com>
2020-10-07 08:25:55 -0300

    function cards

--- jogo-da-memoria/script.js
+++ jogo-da-memoria/script.js
@@ -1,7 +1,27 @@
 const cards = document.querySelectorAll('.card');
+let hasFlippeCard = false;
+let firstCard, secondCard;
 
 function flipCard() {
    this.classList.toggle('flip');
+    if(!hasFlippeCard) {
+        hasFlippeCard = true;
+        firstCard = this;
+        return        
+    }
+
+    secondCard = this;
+    hasFlippeCard = false;
+    checkForMath();
+}
+
+function checkForMatch() {
+    if(firstCard.dataset.card === secondCard.dataset.card) {
+        disableCards();
+        return;
+    }
+
+    unflipCard();
 }
 
 cards.forEach((card) => {
