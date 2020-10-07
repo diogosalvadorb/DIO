2b7095b61367ab05b005aedf0b5085826ddbee8d
diogo <diogosbarbosa93@gmail.com>
2020-10-07 09:00:37 -0300

    reset board, function suffle

--- jogo-da-memoria/script.js
+++ jogo-da-memoria/script.js
@@ -3,10 +3,12 @@ let hasFlippeCard = false;
 let firstCard, secondCard;
 let lockBoard = false;
 
+//função para virar a carta
 function flipCard() {
     if(lockBoard) return;
+    if(this === firstCard) return;
     
-   this.classList.toggle('flip');
+   this.classList.add('flip');
     if(!hasFlippeCard) {
         hasFlippeCard = true;
         firstCard = this;
@@ -15,9 +17,10 @@ function flipCard() {
 
     secondCard = this;
     hasFlippeCard = false;
-    checkForMath();
+    checkForMatch();
 }
 
+//função que checa se as cartas são iguais
 function checkForMatch() {
     if(firstCard.dataset.card === secondCard.dataset.card) {
         disableCards();
@@ -27,18 +30,41 @@ function checkForMatch() {
     unflipCard();
 }
 
+//função que desabilita as cartas
 function disableCards() {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
 }
 
+//função que desvira as cartas
 function unflipCard() {
+    lockBoard = true;
+
     setTimeout(() => {
         firstCard.classList.remove('flip')
         secondCard.classList.remove('flip')
+
+        resetBoard();
     }, 1500);
 }
 
+//função que reseta o tabuleiro
+function resetBoard(){
+    [hasFlippeCard, lockBoard]=[false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+//função que embaralha as cartas
+(function shuffle() {
+    cards.forEach((card) => {
+        let ramdomPosition = Math.floor(Math.random() * 12);
+        card.style.order = ramdomPosition;
+    })
+})();
+
+//adiciona evento de clique na carta
 cards.forEach((card) => {
     card.addEventListener('click', flipCard)
 })
