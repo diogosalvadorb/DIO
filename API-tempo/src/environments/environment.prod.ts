b23931a86a0b3377dcf652dae7fc581c9c5f2e5a
diogo <diogosbarbosa93@gmail.com>
2020-11-30 22:36:16 -0300

    api tempo

---
+++ API-tempo/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiKey: 'ad174b30bbfe040282afcadf04833574',
+};
---
+++ API-tempo/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiKey: 'ad174b30bbfe040282afcadf04833574',
+};
